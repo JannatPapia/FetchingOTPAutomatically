459078a84a2a8e51c65853d0d69419a7b92752e2
PapiaMCode <papiamcode@gmail.com>
2022-09-12 23:24:15 +0600

    Project setup

---
+++ FetchingOTPAutomatically/View/Verification.swift
@@ -0,0 +1,20 @@
+//
+//  Verification.swift
+//  FetchingOTPAutomatically
+//
+//  Created by Jannatun Nahar Papia on 12/9/22.
+//
+
+import SwiftUI
+
+struct Verification: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Verification_Previews: PreviewProvider {
+    static var previews: some View {
+        Verification()
+    }
+}
