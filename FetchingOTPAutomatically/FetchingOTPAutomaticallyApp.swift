232cd9d228b9ab762d535ee23e7c1557182f0370
PapiaMCode <papiamcode@gmail.com>
2022-09-12 23:17:26 +0600

    Initial Commit

---
+++ FetchingOTPAutomatically/FetchingOTPAutomaticallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchingOTPAutomaticallyApp.swift
+//  FetchingOTPAutomatically
+//
+//  Created by Jannatun Nahar Papia on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct FetchingOTPAutomaticallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
