a7e23204701f92e4d2a14c164005b868cf32a22c
PapiaMCode <papiamcode@gmail.com>
2022-09-13 17:29:03 +0600

    Sending OTP

--- FetchingOTPAutomatically/ViewModel/OTPViewModel.swift
+++ FetchingOTPAutomatically/ViewModel/OTPViewModel.swift
@@ -9,8 +9,35 @@ import SwiftUI
 import Firebase
 
 class OTPViewModel: ObservableObject {
+    //MARK: Login Data
+    @Published var number : String = ""
+    @Published var code : String = ""
+    
     @Published var otpText: String = ""
     @Published var otpFields: [String] = Array(repeating: "", count: 6)
+    
+    
+    //MARK: Error
+    @Published var showAlert: Bool = false
+    @Published var errorMsg: String = ""
+    
+    //MARK: Sending OTP
+    func sendOTP()async{
+        do{
+            let result = try await
+            PhoneAuthProvider.provider().verifyPhoneNumber("+880\(code)\(number)", uiDelegate: nil)
+        }
+        catch{
+            handleError(error: error.localizedDescription)
+        }
+    }
+    
+    func handleError(error: String) {
+        DispatchQueue.main.async {
+            self.errorMsg = error
+            self.showAlert.toggle()
+        }
+    }
 }
 
 
